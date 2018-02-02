3c8d9bf1cbeacbd3bb8165c4bf839e5f62cf0b5d
Roy <roye19@gmail.com>
2018-02-01 20:38:41 -0500

    refactored code

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { Component } from 'react'
+import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import faBars from '@fortawesome/fontawesome-free-solid/faBars'
 
-const Header = () => {
-  return (
-    <header>
-      This is a header
-    </header>
-  )
+import logo from '../images/logo.png'
+
+class Header extends Component {
+  constructor (props) {
+    super(props)
+    this.handleBarClick = this.handleBarClick.bind(this)
+  }
+  handleBarClick (e) {
+    console.log('bar clicked')
+  }
+
+  render () {
+    return (
+      <header className='container'>
+        <img src={logo} alt={'Sparta Plaesent'} />
+        <FontAwesomeIcon icon={faBars} size={'2x'}onClick={this.handleBarClick} />
+      </header>
+    )
+  }
 }
 
 export default Header
