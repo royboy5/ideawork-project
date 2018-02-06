bddf7232a721f7f5482a744497010499add45480
Roy <roye19@gmail.com>
2018-02-06 13:49:12 -0500

    Separated data into sampleDate.js

---
+++ src/sampleData.js
@@ -0,0 +1,69 @@
+import ev1 from './images/ev1.png'
+import ev2 from './images/ev2.png'
+import ev3 from './images/ev3.png'
+import ev4 from './images/ev4.png'
+import ev5 from './images/ev5.png'
+import ev6 from './images/ev6.png'
+import slide1 from './images/slide1.png'
+import slide2 from './images/slide2.png'
+import slide3 from './images/slide3.png'
+
+export const items = [
+  {
+    img: ev1,
+    date: 'FEBRUARY 10 2017',
+    title: 'Bureaux exquisite delightful carefully curated soft power.',
+    presentedBy: 'Lorem Ipsum'
+  },
+  {
+    img: ev2,
+    date: 'FEBRUARY 2 2017',
+    title: 'Sharp bureaux sleepy K-pop carefully curated.',
+    presentedBy: 'Lorem Ipsum'
+  },
+  {
+    img: ev3,
+    date: 'JANUARY 27 2017',
+    title: 'St Moritz uniforms Beams.',
+    presentedBy: 'Lorem Ipsum'
+  },
+  {
+    img: ev4,
+    date: 'JANUARY 21 2017',
+    title: 'Esse airport veniam ryokan soft power.',
+    presentedBy: 'Lorem Ipsum'
+  },
+  {
+    img: ev5,
+    date: 'JANUARY 18 2017',
+    title: 'K-pop extraordinary.',
+    presentedBy: 'Lorem Ipsum'
+  },
+  {
+    img: ev6,
+    date: 'JANUARY 12 2017',
+    title: 'Artisanal iconic cutting-edge business class.',
+    presentedBy: 'Lorem Ipsum'
+  }
+]
+
+export const slides = [
+  {
+    image: slide1,
+    title: 'Screening: \n DVF Secret Agent Part 2',
+    date: 'October 15, 2017',
+    text: 'Join us for a private screening of the DVF Secret Agent Part 2 with our special guest, director Peter Lindberg.'
+  },
+  {
+    image: slide2,
+    title: 'Screening: \n DVF Secret Agent Part 2',
+    date: 'October 15, 2017',
+    text: 'Join us for a private screening of the DVF Secret Agent Part 2 with our special guest, director Peter Lindberg.'
+  },
+  {
+    image: slide3,
+    title: 'Screening: \n DVF Secret Agent Part 2',
+    date: 'October 15, 2017',
+    text: 'Join us for a private screening of the DVF Secret Agent Part 2 with our special guest, director Peter Lindberg.'
+  }
+]
