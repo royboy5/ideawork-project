34bd1fb9d766eaaac063258a8ef656428e289218
Roy <roye19@gmail.com>
2018-02-06 18:30:51 -0500

    Added readme button to grid item

--- src/pages/home/GridBox.js
+++ src/pages/home/GridBox.js
@@ -5,9 +5,11 @@ const GridBox = (props) => {
   return (
     <figure className='grid-item' key={props.gridItem.key}>
       <img src={props.gridItem.img} alt={'props.gridItems.title'} onLoad={props.imgLoad} />
-      <div>{props.gridItem.date}</div>
-      <h1>{props.gridItem.title}</h1>
-      <div>Present by <a href={'#'}>{props.gridItem.presentedBy}</a></div>
+      <div className={'grid-item-date'}>{props.gridItem.date}</div>
+      <a className={'grid-item-title'} href={'#'}>{props.gridItem.title}</a>
+      <div className={'grid-item-by'}>Present by <a href={'#'}>{props.gridItem.presentedBy}</a></div>
+      <hr />
+      <div className={'grid-item-readme'}><a href='#'>Read Me</a></div>
     </figure>
   )
 }
