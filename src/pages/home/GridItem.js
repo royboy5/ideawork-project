a2fa76968df2a53c05a7f49daf9fa7746bb8fe56
Roy <roye19@gmail.com>
2018-02-07 11:27:43 -0500

    Updated mobile styles

--- src/pages/home/GridItem.js
+++ src/pages/home/GridItem.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 const GridItem = (props) => {
   return (
     <figure className='grid-item' key={props.gridItem.key}>
-      <a href={'#'}><img src={props.gridItem.img} alt={'props.gridItems.title'} onLoad={props.imgLoad} /></a>
+      <a href={'#'}><img className={'grid-item-img'} src={props.gridItem.img} alt={'props.gridItems.title'} onLoad={props.imgLoad} /></a>
       <div className={'grid-item-date'}>{props.gridItem.date}</div>
       <a className={'grid-item-title'} href={'#'}>{props.gridItem.title}</a>
       <div className={'grid-item-by'}>Present by <a href={'#'}>{props.gridItem.presentedBy}</a></div>
