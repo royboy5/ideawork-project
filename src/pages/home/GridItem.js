62bb1da34fd5c722bb4aced0b79be00d22a744a8
Roy <roye19@gmail.com>
2018-02-07 00:51:02 -0500

    Made image in grid item a link

--- src/pages/home/GridItem.js
+++ src/pages/home/GridItem.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 const GridItem = (props) => {
   return (
     <figure className='grid-item' key={props.gridItem.key}>
-      <img src={props.gridItem.img} alt={'props.gridItems.title'} onLoad={props.imgLoad} />
+      <a href={'#'}><img src={props.gridItem.img} alt={'props.gridItems.title'} onLoad={props.imgLoad} /></a>
       <div className={'grid-item-date'}>{props.gridItem.date}</div>
       <a className={'grid-item-title'} href={'#'}>{props.gridItem.title}</a>
       <div className={'grid-item-by'}>Present by <a href={'#'}>{props.gridItem.presentedBy}</a></div>
